842105cbc37aee29f007104a2bd73216ea137e33
SAAD-DASTGIR <msaaddastgir@gmail.com>
2024-02-16 14:30:46 +0500

    added middleware

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const PORT = 5000;
 app.use(cors());
 
 // Enable file uploads
-app.use(fileUpload());
 app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com;");
   next();
 });
+app.use(fileUpload());
 
 // Initialize Google Generative AI
 const genAI = new GoogleGenerativeAI("AIzaSyD8lt6NXXNklYRfzIx5WJNk8iQimjb5MsU");
